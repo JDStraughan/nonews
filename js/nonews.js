848d47695d4233e3b24dd08b956e94d7d1483e65
JDStraughan <jdstraughan@gmail.com>
2013-01-24 22:51:54 -0600

    Refactoring of blacklists in prep for options page

--- js/nonews.js
+++ js/nonews.js
@@ -6,14 +6,17 @@
     NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
 */
 
-// Get the file for the blacklist to load from json
-var fileURL = chrome.extension.getURL("../assets/blacklist.json"); 
-var xmlreq = new XMLHttpRequest();
-xmlreq.open("GET", fileURL, false);
-xmlreq.send();
+if (typeof localStorage.nonews_blacklist_urls === "undefined") {
+  // Get the file for the blacklist to load from json
+  var fileURL = chrome.extension.getURL("../assets/blacklist.json"); 
+  var xmlreq = new XMLHttpRequest();
+  xmlreq.open("GET", fileURL, false);
+  xmlreq.send();
 
-// The file should be in xmlreq.responseText array
-var blacklist = json_parse(xmlreq.responseText);
+  // The file should be in xmlreq.responseText array
+  blacklist = json_parse(xmlreq.responseText);
+  localStorage.nonews_blacklist_urls = JSON.stringify(blacklist.urls);
+}
 
 // Add the event listener with filters
 chrome.webRequest.onBeforeRequest.addListener(
@@ -24,7 +27,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     }
     // Check for snooze option
     now = new Date().getTime();
-    if (localStorage.nonoews_temp_bypass > now) {
+    if (localStorage.nonews_temp_bypass > now) {
       return;
     }
     // Redirect to blocked site page, passing url for bypass option
@@ -34,7 +37,7 @@ chrome.webRequest.onBeforeRequest.addListener(
   },
   // filters
   {
-    urls: blacklist.urls,
+    urls: JSON.parse(localStorage.nonews_blacklist_urls),
     types: ["main_frame"]
   },
   // extratabSpec
